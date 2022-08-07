62bf8c87f039047513d205785ae5e172511f2686
Aaron Krumsieg <a.krumsieg@gmail.com>
2022-08-07 16:57:08 -0600

    more random changes to main

--- GitBranchingPractice/Program.cs
+++ GitBranchingPractice/Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("I'll be quiet. I'll be peace.");
 Console.WriteLine("another change in the seventh branch");
+Console.WriteLine("another change from eighth branch");
