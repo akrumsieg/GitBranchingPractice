4d91f7b1c2cffd33f702754bdec38babfce996fc
Aaron Krumsieg <a.krumsieg@gmail.com>
2022-08-06 18:21:44 -0600

    add abstract vehicle class

---
+++ GitBranchingPractice/Vehicle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitBranchingPractice
+{
+    internal abstract class Vehicle
+    {
+        public string Name { get; set; }
+
+        public virtual void Go()
+        {
+            Console.WriteLine("Vehicle is going");
+        }
+
+        public virtual void Stop()
+        {
+            Console.WriteLine("Vehicle has stopped");
+        }
+    }
+}
